480e48733b79ca4b2bea61c0033d2ea2ad4cba10
eksrude <eksrud0099@cu.ac.kr>
2022-05-22 23:49:03 +0900

    처음 완성본

---
+++ app/src/main/java/com/example/healingmate/SecretActivity.java
@@ -0,0 +1,63 @@
+package com.example.healingmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class SecretActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secret);
+
+        ImageButton mainbutton1 = (ImageButton) findViewById(R.id.mainbutton1);
+        mainbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton sharebutton1 = (ImageButton) findViewById(R.id.sharebutton1);
+        sharebutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ShareActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton secretbutton1 = (ImageButton) findViewById(R.id.secretbutton1);
+        secretbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SecretActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton bookbutton1 = (ImageButton) findViewById(R.id.bookbutton1);
+        bookbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BookActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageButton settingbutton1 = (ImageButton) findViewById(R.id.settingbutton1);
+        settingbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
